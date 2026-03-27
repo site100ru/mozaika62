bdd34a7cc4ce7297a30ab931ec524a507dd457f9
/usr/bin/vim <support@beget.ru>
2026-03-27 15:16:42 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -243,56 +243,6 @@ function my_woocommerce_get_breadcrumb($breadcrumb) {
 
 
 
-// Register post type
-add_action( 'init', 'register_post_types' );
-function register_post_types() {
-	
-	// Add thumbnails
-	add_theme_support('post-thumbnails');
-	
-	// Тип записи - наши работы (портфолио)
-	register_post_type( 'portfolio', [
-		'label'  => null,
-		'labels' => [
-			'name'               => 'Наши работы', // основное название для типа записи
-			'singular_name'      => 'Наши работы', // название для одной записи этого типа
-			'add_new'            => 'Добавить нашу работу', // для добавления новой записи
-			'add_new_item'       => 'Добавление нашей работы', // заголовка у вновь создаваемой записи в админ-панели.
-			'edit_item'          => 'Редактирование нашей работы', // для редактирования типа записи
-			'new_item'           => 'Новая наша работа', // текст новой записи
-			'view_item'          => 'Смотреть нашу работу', // для просмотра записи этого типа.
-			'search_items'       => 'Искать нашу работу', // для поиска по этим типам записи
-			'not_found'          => 'Не найдено', // если в результате поиска ничего не было найдено
-			'not_found_in_trash' => 'Не найдено в корзине', // если не было найдено в корзине
-			'parent_item_colon'  => '', // для родителей (у древовидных типов)
-			'menu_name'          => 'Наши работы', // название меню
-		],
-		'description'         => '',
-		'public'              => true,
-		// 'publicly_queryable'  => null, // зависит от public
-		// 'exclude_from_search' => null, // зависит от public
-		// 'show_ui'             => null, // зависит от public
-		// 'show_in_nav_menus'   => null, // зависит от public
-		'show_in_menu'        => null, // показывать ли в меню адмнки
-		// 'show_in_admin_bar'   => null, // зависит от show_in_menu
-		'show_in_rest'        => null, // добавить в REST API. C WP 4.7
-		'rest_base'           => null, // $post_type. C WP 4.7
-		'menu_position'       => null,
-		'menu_icon'           => null,
-		//'capability_type'   => 'post',
-		//'capabilities'      => 'post', // массив дополнительных прав для этого типа записи
-		//'map_meta_cap'      => null, // Ставим true чтобы включить дефолтный обработчик специальных прав
-		'hierarchical'        => false,
-		'supports'            => [ 'title', 'editor' ], // 'title','editor','author','trackbacks','comments', 'thumbnail', 'custom-fields','revisions','page-attributes','post-formats', 'excerpt'
-		'taxonomies'          => [ 'portfolio-cat' ],
-		'has_archive'         => true,
-		'rewrite'             => true,
-		'query_var'           => true,
-	] );
-}
-
-
-
 /* Wijet область в сайдбаре */
 if ( function_exists( 'register_sidebar' ) ) {
 	register_sidebar(
