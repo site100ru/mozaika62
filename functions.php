2a81007bf8a49202e48ac8d0f58f63031a22f977
/usr/bin/vim <support@beget.ru>
2026-03-27 15:03:26 +0300

    Del include jQuery

--- functions.php
+++ functions.php
@@ -119,7 +119,8 @@ add_action( 'after_setup_theme', function() {
 	] );
 } );
 
-wp_enqueue_script('jquery');
+
+
 
 function remove_wp_footer_credit() {
 	remove_action('wp_footer', 'wp_admin_footer'); 
@@ -240,49 +241,6 @@ function my_woocommerce_get_breadcrumb($breadcrumb) {
 }
 
 
-
-/* Register taxonomy
-add_action( 'init', 'create_taxonomy' );
-function create_taxonomy() {
-	
-	// Таксономия - портфолио
-	register_taxonomy( 'portfolio-cat', [ 'portfolio' ], [
-		'label'                 => '', // определяется параметром $labels->name
-		'labels'                => [
-			'name'              => 'Наши работы',
-			'singular_name'     => 'Категория портфолио',
-			'search_items'      => 'Искать категорию портфолио',
-			'all_items'         => 'Все категории портфолио',
-			'view_item '        => 'View Genre',
-			'parent_item'       => 'Parent Genre',
-			'parent_item_colon' => 'Parent Genre:',
-			'edit_item'         => 'Edit Genre',
-			'update_item'       => 'Update Genre',
-			'add_new_item'      => 'Add New Genre',
-			'new_item_name'     => 'New Genre Name',
-			'menu_name'         => 'Категории портфолио',
-			'back_to_items'     => '← Вернуться к категориям портфолио',
-		],
-		'description'           => '', // описание таксономии
-		'public'                => true,
-		// 'publicly_queryable'    => null, // равен аргументу public
-		// 'show_in_nav_menus'     => true, // равен аргументу public
-		// 'show_ui'               => true, // равен аргументу public
-		// 'show_in_menu'          => true, // равен аргументу show_ui
-		// 'show_tagcloud'         => true, // равен аргументу show_ui
-		// 'show_in_quick_edit'    => null, // равен аргументу show_ui
-		'hierarchical'          => true,
-		'rewrite'               => true,
-		//'query_var'             => $taxonomy, // название параметра запроса
-		'capabilities'          => array(),
-		'meta_box_cb'           => null, // html метабокса. callback: `post_categories_meta_box` или `post_tags_meta_box`. false — метабокс отключен.
-		'show_admin_column'     => false, // авто-создание колонки таксы в таблице ассоциированного типа записи. (с версии 3.5)
-		'show_in_rest'          => null, // добавить в REST API
-		'rest_base'             => null, // $taxonomy
-		// '_builtin'              => false,
-		//'update_count_callback' => '_update_post_term_count',
-	] );
-}*/
 
 
 // Register post type
@@ -448,13 +406,6 @@ function child_enqueue_styles() {
 }
 
 
-
-
-/*** ВКЛЮЧАЕМ EXCERPT FOR PAGES (ОПИСАНИЕ ДЛЯ СТРАНИЦ)
-add_action('init', 'add_excerpt_to_pages');
-function add_excerpt_to_pages() {
-	add_post_type_support('page', 'excerpt');
-} ***/
 
 // Фильтр для виджета категорий товаров
 function filter_product_categories_widget($list_args)
