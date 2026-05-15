8e16939562ac4483dba64722f4496198cf015511
/usr/bin/vim <support@beget.ru>
2026-05-15 21:34:00 +0300

    Update functions.php

--- functions.php
+++ functions.php
@@ -135,47 +135,6 @@ function furniture_catalog_add_woocommerce_support() {
 	add_theme_support( 'woocommerce' );
 } */
 
-
-/* Изменяем размер миниатюр WooCommerce */
-add_filter('woocommerce_get_image_size_thumbnail','add_thumbnail_size',1,10);
-function add_thumbnail_size($size){
-	$size['width'] = 600;
-	//$size['height'] = 450;
-	$size['crop']   = 1; //0 - не обрезаем, 1 - обрезка
-	return $size;
-}
-
-/* Отключаем ненужные опции вывода настраницу */
-/* Отключаем название товара
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_rating', 10 );
-
-
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 20);
-add_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 20);
-add_action('woocommerce_single_product_summary', 'woocommerce_template_single_excerpt', 10);
-
-
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_upsell_display', 15 ); */
-remove_action( 'woocommerce_after_single_product_summary', 'woocommerce_output_product_data_tabs', 10 );
-remove_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_meta', 40 );
-
-
-// Цена
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_price', 10);
-//add_action( 'woocommerce_single_product_summary', 'woocommerce_template_single_price', 20 );
-
-// Кнопка
-remove_action('woocommerce_single_product_summary', 'woocommerce_template_single_add_to_cart', 30);
-
-/* Изменям значек валюты */
-add_filter('woocommerce_currency_symbol', 'change_existing_currency_symbol', 10, 2);
-function change_existing_currency_symbol( $currency_symbol, $currency ) {
-		switch( $currency ) {
-			case 'RUB': $currency_symbol = '₽'; break;
-		}
-		return $currency_symbol;
-}
 	
 	
 	
